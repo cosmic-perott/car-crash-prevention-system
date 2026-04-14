45c0d745a452f9e1c0a6f3f2ab51e4abf3ab5edb
cosmic_perott <142322366+cosmic-perott@users.noreply.github.com>
2026-04-15 08:55:47 +0900

    Update objectdetection.py

--- object_detection_model/objectdetection.py
+++ object_detection_model/objectdetection.py
@@ -1,1 +1,18 @@
+from ultralytics import YOLO
+import cv2
 
+model = YOLO("runs/detect/train/weights/best.pt")
+
+image_path = "yolo_car_200/images/train/000000567149.jpg" 
+
+results = model(image_path)
+
+annotated = results[0].plot()
+
+cv2.imshow("Detection", annotated)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+cv2.imwrite("output.jpg", annotated)
+
+print("Saved as output.jpg")
